4d65e9aa89fc4ff55eeea04f72e345b4c6c69320
miguel-angelo-nave <miguel.angelo@nave.org.br>
2026-03-16 13:51:16 -0300

    Miguel Angelo

--- ex-02-variaveis-tipos.js
+++ ex-02-variaveis-tipos.js
@@ -37,35 +37,35 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export let musicaFavorita
+export let musicaFavorita = "Jetski"
 
 /* Questão 02 */
-export let objetoQualquer
+export let objetoQualquer = [1, 2, 3, 4]
 
 /* Questão 03 */
-export let valorIndefinido
+export let valorIndefinido 
 
 /* Questão 04 */
-export let tartarugasVoamVerdadeOuNao
+export let tartarugasVoamVerdadeOuNao = false
 
 /* Questão 05 */
-export let noveDivididoPorTres
+export let noveDivididoPorTres = 3;
 
 /* Questão 06 */
-export let papagaiosFalamVerdadeOuNao
+export let papagaiosFalamVerdadeOuNao = true
 
 /* Questão 07 */
 export let umaFuncao
 
 /* Questão 08 */
-export let cemDivididoPorCinco
+export let cemDivididoPorCinco = 20;
 
 /* Questão 09 */
 
-export let nomeDaSuaEscola
+export let nomeDaSuaEscola = "ETE Cícero Dias"
 /* Questão 10 */
 
-export let quinzeMaisCinco
+export let quinzeMaisCinco = 20;
 /* Questão 11 */
 
 export let valorFalso
