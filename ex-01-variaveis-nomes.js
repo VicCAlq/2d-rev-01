8f0af6ca10fad9b50793150347be68c4e042a5b7
Breno <breno.cavalcanti@nave.org.br>
2026-03-16 14:26:17 -0300

    atividade

--- ex-01-variaveis-nomes.js
+++ ex-01-variaveis-nomes.js
@@ -83,46 +83,46 @@ export const dataEHorario = true
 export const videoAula = true
 
 /* Questão 06 */
-export let arquivo_principal = true
+export let arquivoPrincipal = true
 
 /* Questão 07 */
-export class CômodosDaCasa {}
+export class ComodosDaCasa {}
 
 /* Questão 08 */
-export let $4rodas = true
+export let quatroRodas = true
 
 /* Questão 09 */
-export let _5PowerRangers = true
+export let cincoPowerRangers = true
 
 /* Questão 10 */
-export let puro_Aço = true
+export let puroAco = true
 
 /* Questão 11 */
-export const $100reaisEmBarras_de_OURO = true
+export const cemReaisEmBarrasDeOuro = true
 
 /* Questão 12 */
-export const páginaInicial = true
+export const paginaInicial = true
 
 /* Questão 13 */
-export let _5patinhosForamPassear = true
+export let cincoPatinhosForamPassear = true
 
 /* Questão 14 */
-export class $1besouro {}
+export class UmBesouro {}
 
 /* Questão 15 */
-export class Roupas_Juninas {}
+export class RoupasJuninas {}
 
 /* Questão 16 */
-export let cálculosMatemáticos = true
+export let calculosMatematicos = true
 
 /* Questão 17 */
-export const camaMesa_E_Banho = true
+export const camaMesaEBanho = true
 
 /* Questão 18 */
-export let preçoDOproduto = true
+export let precoDoProduto = true
 
 /* Questão 19 */
-export const código_fonte = true
+export const codigoFonte = true
 
 /* Questão 20 */
-export class Cabo_frio {}
+export class CaboFrio {}
--- ex-02-variaveis-tipos.js
+++ ex-02-variaveis-tipos.js
@@ -52,47 +52,47 @@ export let tartarugasVoamVerdadeOuNao = false
 export let noveDivididoPorTres = 3
 
 /* Questão 06 */
-export let papagaiosFalamVerdadeOuNao
+export let papagaiosFalamVerdadeOuNao = true
 
 /* Questão 07 */
-export let umaFuncao
+export let umaFuncao = function(){}
 
 /* Questão 08 */
-export let cemDivididoPorCinco
+export let cemDivididoPorCinco = 20
 
 /* Questão 09 */
 
-export let nomeDaSuaEscola
+export let nomeDaSuaEscola = "ETE cicero dias"
 /* Questão 10 */
 
-export let quinzeMaisCinco
+export let quinzeMaisCinco = 20
 /* Questão 11 */
 
-export let valorFalso
+export let valorFalso = false
 /* Questão 12 */
 
-export let dezMaisQuatro
+export let dezMaisQuatro = 14
 
 /* Questão 13 */
-export let nomeDoSeuEstado
+export let nomeDoSeuEstado = "Pernambuco"
 
 /* Questão 14 */
-export let artistaFavorito
+export let artistaFavorito = "boywithuke"
 
 /* Questão 15 */
-export let seteVezesSete
+export let seteVezesSete = 49
 
 /* Questão 16 */
-export let umMaiorQueDoisVerdadeOuNao
+export let umMaiorQueDoisVerdadeOuNao = false
 
 /* Questão 17 */
-export let comidaFavorita
+export let comidaFavorita = "cuscuz"
 
 /* Questão 18 */
-export let oCeuEhAzulVerdadeOuNao
+export let oCeuEhAzulVerdadeOuNao = false
 
 /* Questão 19 */
-export let objetoNulo
+export let objetoNulo = null
 
 /* Questão 20 */
-export let valorVerdadeiro
+export let valorVerdadeiro = true
