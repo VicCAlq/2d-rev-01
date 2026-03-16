561da968827170a92e2092e9d53f172f280d6751
gabriel-cavalcanti-nave <gabriel.cavalcanti@nave.org.br>
2026-03-16 13:42:18 -0300

    completo

--- ex-04-operadores-aritimeticos.js
+++ ex-04-operadores-aritimeticos.js
@@ -37,28 +37,28 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 8
 */
 
 /* Questão 01 */
-export const trintaENove = 44 ?? 10 ?? 5
+export const trintaENove = 44 - 10 + 5
 
 /* Questão 02 */
-export const centoESessentaEUm = (13 ?? 2) ?? 8
+export const centoESessentaEUm = (13 ** 2) - 8
 
 /* Questão 03 */
-export const trintaECinco = 7 ?? 5
+export const trintaECinco = 7 * 5
 
 /* Questão 04 */
-export const centoEVinteESeis = (22 ?? 6) ?? (24 ?? 4)
+export const centoEVinteESeis = (22 * 6) - (24 / 4)
 
 /* Questão 05 */
-export const oitentaEQuatro = (200 ?? 4) ?? 34
+export const oitentaEQuatro = (200 / 4) + 34
 
 /* Questão 06 */
-export const sessentaEQuatro = 2 ?? (2 ?? 3)
+export const sessentaEQuatro = 2 ** (2 * 3)
 
 /* Questão 07 */
-export const vinteETres = 18 ?? (33 ?? 7)
+export const vinteETres = 18 + (33 % 7)
 
 /* Questão 08 */
-export const dezenove = 38 ?? 2
+export const dezenove = 38 / 2
 
 /* Questão 09 */
 export const centoETrinta = (41 ?? 3) ?? (21 ?? 3)
